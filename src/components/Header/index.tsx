c71d007d7fb3497ab16868d1570822ae7e44f165
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-07-25 19:36:19 -0300

    feat: componente header finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,22 @@
 import { HeaderContainer } from "./styles";
 
 import logo from "../../assets/logo.png";
+import { MapPin, ShoppingCart } from "@phosphor-icons/react";
+import { NavLink } from "react-router-dom";
 
 export function Header(){
     return (
         <HeaderContainer>
             <img src={logo} alt="Logo da Marca Coffee Delivery" />
-            <section>
-                <button></button>
-                <button></button>
-            </section>
+            <nav>
+                <span>
+                    <MapPin size={22} weight="fill" />
+                    <p>Fortaleza, CE</p>
+                </span>
+                <NavLink to="/checkout" title="Checkout de Produtos">
+                    <ShoppingCart size={22} weight="fill" />
+                </NavLink>
+            </nav>
         </HeaderContainer>
     );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,38 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 32px 0 32px 0;
 
-`; 
+    nav{
+        display: flex;
+        align-items: center;
+        gap: 12px;
+
+        span{
+            display: flex;
+            align-items: center;
+            gap: 2px;
+            color: ${props => props.theme.purple};
+            background-color: ${props => props.theme["purple-light"]};
+            padding: 8px;
+            border-radius: 6px;
+            
+            p{
+                color: ${props => props.theme["purple-dark"]};
+            }
+        }
+
+        a{
+            display: flex;
+            align-items: center;
+            text-decoration: none;
+            color: ${props => props.theme["yellow-dark"]};
+            background-color: ${props => props.theme["yellow-light"]};
+            padding: 8px;
+        }
+
+    }
+`;
