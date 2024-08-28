c00d8c81f02469277a2f11248f9a093133f86155
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-08-27 21:30:16 -0300

    feat: renderizando dinâmicamente a home usando contexto

--- src/components/InputQuatitySelection/index.tsx
+++ src/components/InputQuatitySelection/index.tsx
@@ -1,13 +1,13 @@
 import { InputCotainer } from "./styles";
 
-export function InputQuantitySelection(){
+export function InputQuantitySelection(value : number){
     return (
         <InputCotainer>
             <button>
                 -
             </button>
 
-            <input type="number" placeholder="0" min={0} />
+            <input type="number" placeholder="0" value={value} min={0} />
 
             <button>
                 +
