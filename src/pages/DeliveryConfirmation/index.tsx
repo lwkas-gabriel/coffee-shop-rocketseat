7653bbb3fd1a900bdf17b2582d225c01a167e0ac
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-09-09 18:19:10 -0300

    css: refatorando estilos da página DeliveryConfirmation

--- src/pages/DeliveryConfirmation/index.tsx
+++ src/pages/DeliveryConfirmation/index.tsx
@@ -1,5 +1,6 @@
+import { MapPin } from "@phosphor-icons/react";
 import image  from "../../assets/delivery.png";
-import { Container, DeliveryInfoContainer, InfoHeader } from "./styles";
+import { Bullet, Container, DeliveryInfoContainer, InfoHeader } from "./styles";
 
 export function DeliveryConfirmation(){
     return (
@@ -8,12 +9,38 @@ export function DeliveryConfirmation(){
                 <h2>Uhu! Pedido confirmado</h2>
                 <p>Agora é só aguardar que logo o café chegará até você</p>
             </InfoHeader>
-            <DeliveryInfoContainer>
-                <div>
-                    <div></div>
-                </div>
+            <section>
+                <DeliveryInfoContainer>
+                    <section>
+                        <Bullet className="purple">
+                            <MapPin size={16} weight="fill"/>
+                        </Bullet>
+                        <div>
+                            <p>Entrega em <strong>Rua Porto Alegre, 1701</strong></p>
+                            <p>Autran Nunes, Porto Alegre, RS</p>
+                        </div>
+                    </section>
+                    <section>
+                        <Bullet className="orange">
+                            <MapPin size={16} weight="fill"/>
+                        </Bullet>
+                        <div>
+                            <p>Previsão de Entrega</p>
+                            <p><strong>20 min - 30 min</strong></p>
+                        </div>
+                    </section>
+                    <section>
+                        <Bullet className="yellow">
+                            <MapPin size={16} weight="fill"/>
+                        </Bullet>
+                        <div>
+                            <p>Pagamento na entrega</p>
+                            <p><strong>Cartão de Crédito</strong></p>
+                        </div>
+                    </section>
+                </DeliveryInfoContainer>
                 <img src={image} alt="" />
-            </DeliveryInfoContainer>
+            </section>
         </Container>
     );
 }
--- src/pages/DeliveryConfirmation/styles.ts
+++ src/pages/DeliveryConfirmation/styles.ts
@@ -3,24 +3,52 @@ import styled from "styled-components";
 export const Container = styled.main`
     padding-top: 100px;
     display: flex;
+    width: 100%;
+    align-items: center;
     flex-direction: column;
-    justify-content: center;
+
+    >section{
+        display: flex;
+        justify-content: center;
+        width: 80%;
+        justify-content: space-evenly;
+        /* gap: 32px; */
+
+        @media (max-width: 1024px){
+            flex-direction: column-reverse;
+            align-items: center;
+        }
+    }
 `
 
 export const DeliveryInfoContainer = styled.section`
     display: flex;
-    flex-direction: row;
+    width: 30%;
+    flex-direction: column;
     justify-content: space-evenly;
-    
-    >div{
-        background: linear-gradient(white, white) padding-box, linear-gradient(to right, #DBAC2C, #8047F8) border-box;
-        border-top-left-radius: 8px;
-        border-bottom-right-radius: 8px;
-        border-top-right-radius: 36px;
-        border-bottom-left-radius: 36px;
-        border: 1px solid transparent;
-        padding: auto auto;
+    font-size: 1rem;
+    background: linear-gradient(white, white) padding-box, linear-gradient(to right, #DBAC2C, #8047F8) border-box;
+    border-top-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    border-top-right-radius: 36px;
+    border-bottom-left-radius: 36px;
+    border: 1px solid transparent;
+    padding: auto auto;
+    gap: 32px;
+
+    @media (max-width: 1024px){
+        max-width: 450px;
     }
+
+        >section{
+            display: flex;
+            width: fit-content;
+            flex-direction: row;
+            align-items: center;
+            height: fit-content;
+            gap: 5px;
+            padding: 0px 40px 0 40px;
+        }
 
 `
 
@@ -36,5 +64,31 @@ export const InfoHeader = styled.div`
         margin-bottom: 40px;
         color: ${props => props.theme["base-subtitle"]};
         font-size: 20px;
+    }
+`
+
+export const Bullet = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 8px;
+    width: 32px;
+    height: 32px;
+    border-radius: 1000px;
+    color:  ${props => props.theme["white"]};
+
+    &.orange{
+        background-color: ${props => props.theme["yellow-dark"]};
+    }
+
+    &.black{
+        background-color: ${props => props.theme["base-text"]};
+    }
+
+    &.yellow{
+        background-color: ${props => props.theme.yellow};
+    }
+
+    &.purple{
+        background-color: ${props => props.theme["purple"]};
     }
 `
